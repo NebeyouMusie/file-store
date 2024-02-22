120e0f65f867287702df6906b390630f14c75f34
Nebeyou Musie <nebeyoumusie@gmail.com>
2024-02-22 13:11:13 +0300

    Implemented firebase for file storage

--- components/Dropzone.tsx
+++ components/Dropzone.tsx
@@ -1,16 +1,73 @@
 "use client";
 
+import { db, storage } from "@/firebase";
 import { cn } from "@/lib/utils";
+import { useUser } from "@clerk/nextjs";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
+import { useState } from "react";
 import DropzoneComponent from "react-dropzone";
 
 const Dropzone = () => {
   const maxSize = 20971520;
+  const [loading, setLoading] = useState<boolean>(false);
+  const { isLoaded, isSignedIn, user } = useUser();
+
+  const onDrop = (acceptedFiles: File[]) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+
+      reader.onabort = () => console.log("File readingg was aborted");
+      reader.onerror = () => console.log("File reading has failed");
+      reader.onload = async () => {
+        await uploadPost(file);
+      };
+
+      reader.readAsArrayBuffer(file);
+    });
+  };
+
+  const uploadPost = async (selectedFile: File) => {
+    if (loading) return;
+    if (!user) return;
+
+    setLoading(true);
+
+    // addDoc -> users/user12345/files
+    const docRef = await addDoc(collection(db, "users", user.id, "files"), {
+      userId: user.id,
+      filename: selectedFile.name,
+      fullName: user.fullName,
+      profileImg: user.imageUrl,
+      timestamp: serverTimestamp(),
+      type: selectedFile.type,
+      size: selectedFile.size,
+    });
+
+    const imageRef = ref(storage, `users/${user.id}/files/${docRef.id}`);
+
+    uploadBytes(imageRef, selectedFile).then(async (snapshot) => {
+      const downloadURL = await getDownloadURL(imageRef);
+
+      await updateDoc(doc(db, "users", user.id, "files", docRef.id), {
+        downloadURL: downloadURL,
+      });
+    });
+
+    setLoading(false);
+  };
 
   return (
     <DropzoneComponent
       minSize={0}
       maxSize={maxSize}
-      onDrop={(acceptedFiles) => console.log(acceptedFiles)}
+      onDrop={onDrop}
     >
       {({
         getRootProps,
---
+++ firebase.ts
@@ -0,0 +1,21 @@
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCi7MGucBz8ZvZocNsXw14hPTz7oUnk60U",
+  authDomain: "filez-9af7b.firebaseapp.com",
+  projectId: "filez-9af7b",
+  storageBucket: "filez-9af7b.appspot.com",
+  messagingSenderId: "10003218151",
+  appId: "1:10003218151:web:c68e2c35ee5969da5eec42"
+};
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { db, storage};
