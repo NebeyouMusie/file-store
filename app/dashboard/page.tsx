7e9a11fa26b3fbb8d12492b77f806e54d18f011a
Nebeyou Musie <nebeyoumusie@gmail.com>
2024-02-23 14:09:25 +0300

    Added table

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,36 @@
-import Dropzone from "@/components/Dropzone"
+import Dropzone from "@/components/Dropzone";
+import TableWrapper from "@/components/table/TableWrapper";
+import { db } from "@/firebase";
+import { FileType } from "@/typings";
+import { auth } from "@clerk/nextjs";
+import { collection, getDocs } from "firebase/firestore";
 
-const Dashboard = () => {
+const Dashboard = async () => {
+  const { userId } = auth();
+
+  const docsResults = await getDocs(collection(db, "users", userId!, "files"));
+  const skeletonFiles: FileType[] = docsResults.docs.map((doc) => ({
+    id: doc.id,
+    filename: doc.data().filename || doc.id,
+    timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+    fullName: doc.data().fullName,
+    downloadURL: doc.data().downloadURL,
+    type: doc.data().type,
+    size: doc.data().size,
+  }));
+  // console.log(skeletonFiles);
   return (
     <div>
       <Dropzone />
-    </div>
-  )
-}
 
-export default Dashboard
+      <section className="space-y-5 container">
+        <h2 className="font-bold">All Files</h2>
+        <div>
+          <TableWrapper skeletonFiles={skeletonFiles}/>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ constant.ts
@@ -0,0 +1,17 @@
+export const COLOR_EXTENSION_MAP: {
+  [key: string]: string;
+} = {
+  pdf: "#0160fe",
+  docx: "#2B579a",
+  doc: "#2b579a",
+  pptx: "#b7472a",
+  ppt: "#b7472a",
+  xlsx: "#217346",
+  mpeg: "#000000",
+  xls: "#217346",
+  jpg: "#d4af37",
+  jpeg: "#d4af37",
+  png: "#4fb6f4",
+  gif: "#d4af37",
+  txt: "#00000",
+}
